b9c1f5586bdc437e1d41946ce84a9e76b098044f
Dmitry Shapovalov <shenf@mail.ru>
2024-04-14 17:37:46 +0300

    Добавил javadoc для пакета

---
+++ src/main/java/ru/d_shap/gradle/plugin/texturepacker/configuration/package-info.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// TexturePacker Gradle Plugin is a plugin to call TexturePacker CLI.
+// Copyright (C) 2024 Dmitry Shapovalov.
+//
+// This file is part of TexturePacker Gradle Plugin.
+//
+// TexturePacker Gradle Plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// TexturePacker Gradle Plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Package contains configuration classes.
+ */
+package ru.d_shap.gradle.plugin.texturepacker.configuration;
