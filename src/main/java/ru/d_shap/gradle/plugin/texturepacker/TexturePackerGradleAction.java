df3519a0b420d9a75329ddfe08ae90cad1bc736c
Dmitry Shapovalov <shenf@mail.ru>
2024-04-14 17:36:04 +0300

    Действие

---
+++ src/main/java/ru/d_shap/gradle/plugin/texturepacker/TexturePackerGradleAction.java
@@ -0,0 +1,61 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// TexturePacker Gradle Plugin is a plugin to call TexturePacker CLI.
+// Copyright (C) 2024 Dmitry Shapovalov.
+//
+// This file is part of TexturePacker Gradle Plugin.
+//
+// TexturePacker Gradle Plugin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// TexturePacker Gradle Plugin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////////////////////////
+package ru.d_shap.gradle.plugin.texturepacker;
+
+import org.gradle.api.Action;
+import org.gradle.api.Task;
+
+import ru.d_shap.gradle.plugin.texturepacker.configuration.ExtensionConfiguration;
+
+/**
+ * TexturePacker gradle action.
+ *
+ * @author Dmitry Shapovalov
+ */
+public class TexturePackerGradleAction implements Action<Task> {
+
+    private static final String COMMAND = "TexturePacker";
+
+    private final ExtensionConfiguration _extensionConfiguration;
+
+    /**
+     * Create new object.
+     *
+     * @param extensionConfiguration the extension configuration.
+     */
+    public TexturePackerGradleAction(final ExtensionConfiguration extensionConfiguration) {
+        super();
+        _extensionConfiguration = extensionConfiguration;
+    }
+
+    @Override
+    public void execute(final Task task) {
+        if (Logger.isWarnEnabled()) {
+            Logger.warn("Start processing images with TexturePacker");
+        }
+        if (_extensionConfiguration != null) {
+            Logger.warn("_extensionConfiguration:" + _extensionConfiguration);
+        }
+        if (Logger.isWarnEnabled()) {
+            Logger.warn("Finish processing images with TexturePacker");
+        }
+    }
+
+}
